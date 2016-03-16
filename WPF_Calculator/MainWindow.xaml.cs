927a51abcc08be445f3abbde59b646b6eadbc1be
Morgan Trench <morgan.trench@gmail.com>
2016-03-17 01:32:46 +1100

    Mostly working simple GUI, operator buttons don't work at the moment.

--- WPF_Calculator/MainWindow.xaml.cs
+++ WPF_Calculator/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+// Debub
+using System.Diagnostics;
+
 namespace WPF_Calculator
 {
     /// <summary>
@@ -20,9 +23,75 @@ namespace WPF_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Value displayed on right of calculator display, used to display the most recent value/operator entered and oputput of the calculation.
+        String inVal = "0";
+        // To be display above and to the left of the input values/result
+        String expressionString = "";
+
+        // TODO
+        // CalculatorTree expression = new CalculatorTree();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void digit(String input)
+        {
+            if (inVal == "0")
+            {
+                inVal = "";
+            }
+            inVal += input;
+            Display.Text = inVal;
+        }
+
+        private void reset(int input)
+        {
+            inVal = "0";
+            Display.Text = inVal;
+        }
+
+        private void Numerical_Button_Click(object sender, RoutedEventArgs e)
+        {
+            String temp = ((Button)sender).Name;
+            Debug.WriteLine(temp + " Clicked");
+            temp = temp.Substring(temp.Length-1);
+            digit(temp);
+        }
+
+        private void Calculator_Function_Button_Click(object sender, RoutedEventArgs e) {
+            Button temp = (Button)sender;
+            switch (temp.Name)
+            {
+                case "AllClear":
+                    inVal = "0";
+                    Display.Text = inVal;
+                    break;
+                case "Point":
+                    inVal += ".";
+                    Display.Text = inVal;
+                    break;
+                default:
+                    Debug.WriteLine("Non Calculator Function Case reached in Calculator_Function_Click");
+                    break;
+            }
+        }
+
+        private void Operator_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button temp = (Button)sender;
+            switch (temp.Name)
+            {
+                case "AllClear":
+                    inVal = "0";
+                    Display.Text = inVal;
+                    break;
+                default:
+                    Debug.WriteLine("Non Operator Case reached in Operator_Click");
+                    break;
+            }
+        }
+
     }
 }
